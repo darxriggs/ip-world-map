7ae71f0cdcbb07673883f84f637cdc2475de1a3b
René Scheibe <rene.scheibe@gmail.com>
2009-04-15 21:04:42 +0200

    added intial specs (not yet error-free)

---
+++ spec/decay.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe PointInTime do
+  it 'should be initialized with either Integers and/or Floats' do
+    Decay.new(10,   5  )
+    Decay.new(10,   5.0)
+    Decay.new(10.0, 5  )
+    Decay.new(10.0, 5.0)
+  end
+
+  it 'should return a value depending on the time' do
+    decay = Decay.new(10, 5)
+
+    decay.value(0).should be 10
+    decay.value(2).should be 0 # todo
+  end
+end
---
+++ spec/point_in_time.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+describe PointInTime do
+  it 'should convert currencies properly' do
+    Currency::convert('AUD', 'USD').should > 0
+  end
+
+  it 'raise an exception on incorrect currencies' do
+    lambda { Currency::convert('YYY', 'XXX') }.should raise_error(Currency::RateError)
+  end
+
+  it 'should process amounts properly' do
+    amount = 10
+    result_with_amount    = Currency::convert('AUD', 'USD', amount)
+    result_without_amount = Currency::convert('AUD', 'USD')
+
+    result_with_amount.should == amount * result_without_amount
+  end
+end
+
