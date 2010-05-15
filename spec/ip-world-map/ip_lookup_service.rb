9a0e02f8d52a5ebb7999156569ade384ad993dae
René Scheibe <rene.scheibe@gmail.com>
2010-05-15 15:48:59 +0200

    refactored code to use IPSocket instead of Socket for hostname -> IP lookup

--- spec/ip-world-map/ip_lookup_service.rb
+++ spec/ip-world-map/ip_lookup_service.rb
@@ -1,47 +1,34 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
 describe IpLookupService do
-  describe 'IP check' do
-    it 'should check if a given string is an IP' do
-      IpLookupService.is_ip?('1.1.1.1').should be true
-      IpLookupService.is_ip?('1.1.1.1234').should be false
-      IpLookupService.is_ip?('no IP').should be false
-    end
-  end
-
-  describe 'hostname -> IP resolval' do
-    it 'should return the IP for an existing hostname' do
-      stubbed_result = [['AF_INET', 0, 'domain.net', '1.1.1.1', 2, 1, 6],
-                        ['AF_INET', 0, 'domain.net', '2.2.2.2', 2, 2, 0]]
-      Socket.stub!(:getaddrinfo).and_return(stubbed_result)
-
-      IpLookupService.ip_for_host('existing hostname').should match /^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/
-    end
-
-    it 'should return the IP if an IP is given as hostname' do
-      IpLookupService.ip_for_host('1.1.1.1').should == '1.1.1.1'
-    end
-
-    it 'should handle the case that no IP can be resolved for a hostname' do
-      Socket.stub!(:getaddrinfo).and_raise(SocketError)
-
-      IpLookupService.ip_for_host('non-existing hostname').should be nil
-    end
-  end
-
   describe 'hostname -> coordinates resolval' do
     it 'should return the coordinates for an IP' do
       stubbed_result = "Country: GERMANY (DE)\nCity: Berlin\n\nLatitude: 52.5\nLongitude: 13.4167\nIP: 1.1.1.1\n"
       Net::HTTP.stub!(:get).and_return(stubbed_result)
-      Socket.stub!(:getaddrinfo)
-   
+
+      IpLookupService.new.coordinates_for_host('1.1.1.1').should == [13.4167, 52.5]
+    end
+
+    it 'should return the coordinates for a hostname' do
+      stubbed_result = "Country: GERMANY (DE)\nCity: Berlin\n\nLatitude: 52.5\nLongitude: 13.4167\nIP: 1.1.1.1\n"
+      Net::HTTP.stub!(:get).and_return(stubbed_result)
+      IPSocket.stub!(:getaddr).and_return('1.1.1.1')
+
       IpLookupService.new.coordinates_for_host('some hostname').should == [13.4167, 52.5]
     end
-   
-    it 'should handle the case that no coordinates can be resolved for an IP' do
+
+    it 'should handle the case that no IP can be resolved for a hostname' do
       stubbed_result = "Country: GERMANY (DE)\nCity: Berlin\n\nLatitude: \nLongitude: \nIP: 1.1.1.1\n"
       Net::HTTP.stub!(:get).and_return(stubbed_result)
-      Socket.stub!(:getaddrinfo)
+      IPSocket.stub!(:getaddrinfo).and_raise(SocketError)
+   
+      IpLookupService.new.coordinates_for_host('some hostname').should == [nil, nil]
+    end
+   
+    it 'should handle the case that no coordinates can be resolved for a hostname' do
+      stubbed_result = "Country: GERMANY (DE)\nCity: Berlin\n\nLatitude: \nLongitude: \nIP: 1.1.1.1\n"
+      Net::HTTP.stub!(:get).and_return(stubbed_result)
+      IPSocket.stub!(:getaddr)
    
       IpLookupService.new.coordinates_for_host('some hostname').should == [nil, nil]
     end
